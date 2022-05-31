db32279c8cde8ea02157208fdfae134db6763bf4
Daniel Rushidov <daniel.rushidov@gmail.com>
2022-05-31 22:20:30 +0300

    Update file conversion result page

--- convert_file.php
+++ convert_file.php
@@ -4,22 +4,47 @@ include_once("parsing_util.php");
 include("yamlToJsonParser.php");
 include("jsonToYamlParser.php");
 
+$source = "";
+$result = "";
+
 if ($_POST) {
     if ($_FILES["file"]["error"] === UPLOAD_ERR_OK && is_uploaded_file($_FILES["file"]["tmp_name"])) {
-        if ($_POST["conversion-type"] === "yaml-to-json") {
-            echo "<textarea rows='40' cols='60'>" . getJsonFromYaml(file_get_contents($_FILES["file"]["tmp_name"])) . "</textarea>";
-        } else if ($_POST["conversion-type"] === "json-to-yaml") {
-            echo "<textarea rows='40' cols='60'>" . getYamlFromJson(file_get_contents($_FILES["file"]["tmp_name"])) . "</textarea>";
-        }
+        $source = file_get_contents($_FILES["file"]["tmp_name"]);
     } else if (!empty($_POST["file-text"])) {
-        if ($_POST["conversion-type"] === "yaml-to-json") {
-            echo "<textarea rows='40' cols='60'>" . getJsonFromYaml($_POST["file-text"]) . "</textarea>";
-        } else if ($_POST["conversion-type"] === "json-to-yaml") {
-            echo "<textarea rows='40' cols='60'>" . getYamlFromJson($_POST["file-text"]) . "</textarea>";
-        }
+        $source = $_POST["file-text"];
     } else {
         // echo "Error. File and content not present";
+    }
+
+    if ($_POST["conversion-type"] === "yaml-to-json") {
+        $result = getJsonFromYaml($source);
+    } else if ($_POST["conversion-type"] === "json-to-yaml") {
+        $result = getYamlFromJson($source);
     }
 }
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Резултат от конвертиране</title>
+    <link rel="stylesheet" href="css/convert_result.css">
+</head>
+<body>
+    <main>
+        <h1>Резултат от конвертиране</h1>
+        <section class="files">
+            <section class="file-container">
+                <h3>Изходен файл</h3>
+                <section class="file-text"><?= $source ?></section>
+            </section>
+            <section class="file-container">
+                <h3>Резултат</h3>
+                <section class="file-text"><?= $result ?></section>
+            </section>
+        </section>
+    </main>
+</body>
+</html>
